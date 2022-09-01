13296ed0f58a5d9edcfe8a297f19fe1f5d8855cd
SerenityK <103469793+SerenityK@users.noreply.github.com>
2022-09-01 15:03:25 -0700

    Add files via upload

---
+++ Twitter_clone/posts/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Post
+from .forms import PostForm
+
+
+def index(request):
+# If the method is POST
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES) 
+    # if the form is Valid
+        if form.is_valid():
+        # Yes, Save
+             form.save()
+        # Redirect to home
+             return HttpResponseRedirect ('/')
+
+        else:
+        # No, Show Error
+            return HttpResponseRedirect(form.errors.as_json())
+
+
+
+
+    # Get all posts, limit = 20
+    posts = Post.objects.all().order_by('-created_at') [:20]
+    form=PostForm()
+
+    # Show
+    return render(request, 'posts.html',
+                {'posts': posts})
+
+def delete(request, post_id):
+    # Find post
+    post = Post.objects.get(id = post_id)
+    post.delete()
+    return HttpResponseRedirect('/')
+               
+# Create your views here.
+def edit(request, post_id):
+    post= Post.objects.get(id=post_id)
+    if request.method =="POST":
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+        else:
+            return HttpResponseRedirect(form.errors.as_json())
+    form = PostForm
+    return render(request, 'edit.html', {'post': post, 'form': form})
+
+def likes(request, post_id):
+    newlikecount = Post.objects.get(id=post_id) 
+    newlikecount.likecount += 1
+    newlikecount.save()
+    return HttpResponseRedirect('/')            
